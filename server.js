d247ed5922b412b1e09d7efc51a085de59567c62
Ngoc Thong Ho <ngocthonggl01@gmail.com>
2025-03-26 18:00:05 +0700

    more-route

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require("express");
-const app = express();
+const express = require("express"); //commondjs
+
+const app = express(); //add express to app
 const port = 8080;
 
+//khai bao route
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/abc", (req, res) => {
+  res.send("Check ABC!");
+});
+
+app.get("/tommy", (req, res) => {
+  res.send("<h1>Tommy dep trai</h1>");
 });
 
 app.listen(port, () => {
